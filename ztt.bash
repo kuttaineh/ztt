faf3522a97eeeaa93c17b64f58f2419c8a28b187
Rammy <kuttaineh@gmail.com>
2025-09-12 14:32:50 -0400

    Update ztt.bash
    Refactored by introducing function presentWord, reducing code length by 36 lines.

--- ztt.bash
+++ ztt.bash
@@ -5,7 +5,7 @@
 # Data files location:
 
 	dfl="$HOME/ztt"
-	one="$dfl/1s.txt"
+	oneletterfile="$dfl/1s.txt"
 	two="$dfl/2s.txt"
 	thr="$dfl/3s.txt"
 	four="$dfl/4s.txt"
@@ -35,6 +35,17 @@
 	  then utter "Very Good!"; echo;
 	  else utter "Try again later."; echo; fi; }
 
+        function presentWord
+        { if [[ -n "$1" ]]; then
+                setrange "$1"
+                word="`sed "${randline}q;d" $1`" # pull a word
+                utter "$word"; echo
+                read -r -p "  Type the word: "
+                check "$REPLY" "$word"
+          else
+                echo "Error: Wordfile $1 not found."
+          fi }
+
 # What prompt to display:
 
 	prompt="How many letters [ 1 2 3 4 5 or 6 ]? "
@@ -51,62 +62,13 @@ while read -n1 -s -p "$prompt "; do
 randline=$RANDOM
 
   case $REPLY in
-    1) echo; echo -n "One letter word: "
-
-	setrange "$one"
-	oneword="`sed "${randline}q;d" $one`"
-	utter "$oneword"; echo
-	read -r -p "  Type the word: "
-	check "$REPLY" "$oneword"
-	;; 		# end case 1
-
-    2) echo; echo -n "Two letter word: "
-
-	setrange "$two"
-	twoword="`sed "${randline}q;d" $two`"
-	utter "$twoword"; echo
-	read -r -p "  Type the word: "
-	check "$REPLY" "$twoword"
-	;;		# end case 2
-
-    3) echo; echo -n "Three letter word: "
-
-	setrange "$thr"
-        threword="`sed "${randline}q;d" $thr`"
-	utter "$threword"; echo
-	read -r -p " Type the word:    "
-	check "$REPLY" "$threword"
-        ;;              # end case 3
-
-    4) echo; echo -n "Four letter word: "
-
-	setrange "$four"
-        fourword="`sed "${randline}q;d" $four`"
-        utter "$fourword"; echo
-        read -r -p " Type the word:   "
-	check "$REPLY" "$fourword"
-        ;;              # end case 4
-
-    5) echo; echo -n "Five letter word: "
-
-	setrange "$five"
-	fiveword="`sed "${randline}q;d" $five`"
-	utter "$fiveword"; echo
-	read -r -p " Type the word:   "
-	check "$REPLY" "$fiveword"
-	;; 		# end case 5
-
-    6) echo; echo -n "Six letter word: "
-
-        setrange "$six"
-        sixword="`sed "${randline}q;d" $six`"
-        utter "$sixword"; echo
-        read -r -p " Type the word:  "
-        check "$REPLY" "$sixword"
-	;;              # end case 6
-
+    1) echo; echo -n "One letter word: "; presentWord "$oneletterfile";;
+    2) echo; echo -n "Two letter word: "; presentWord "$two";;
+    3) echo; echo -n "Three letter word: "; presentWord "$thr";;
+    4) echo; echo -n "Four letter word: "; presentWord "$four";;
+    5) echo; echo -n "Five letter word: "; presentWord "$five";;
+    6) echo; echo -n "Six letter word: "; presentWord "$six";;
     q) say "Goodbye!"; echo "Goodbye!"; break;;
     *) say "$IMPRINT, what?";;
   esac
 done
-
